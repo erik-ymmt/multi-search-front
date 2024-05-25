df343dc67a6a71b59da976f5fa4abb3d21f35df5
Erik Yamamoto <erik.yamamoto@bringitps.com>
2024-05-25 16:55:08 -0300

    Develop header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import './App.css'
-import Search from "./components/Search";
+import "./App.css";
+import Header from "./components/Header";
 import Results from "./components/Results";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-
   return (
-    <main>
-      <Search />
+    <main className="d-flex vh-100 flex-column align-items-center gap-3">
+      <Header />
       <Results />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
